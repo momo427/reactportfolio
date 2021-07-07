333850f86d68241ebdba62f382c37577f1048aaa
momogit <ferguson.tyra@gmail.com>
2021-07-07 13:19:28 -0400

    portfoliojs and devdependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Portfolio from "./portfolio"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Today
-        </a>
-      </header>
-    </div>
-  );
+ <Portfolio />
+  )
 }
 
 export default App;
---
+++ src/portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Portfolio(){
+    return (
+        <>
+        <h1>Ferguson Portfolio</h1>
+        <section> 
+            <div>section one</div>
+            <div>section two</div>
+            <div>section three</div>
+        </section>
+
+    </>
+    )
+}
